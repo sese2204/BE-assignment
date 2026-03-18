de280a9dbe15db6661d8422cad785650ff7363d5
sehyun <sese2204@naver.com>
2026-03-18 21:31:42 +0900

    first commit

---
+++ src/main/kotlin/org/example/beassignment/BeAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package org.example.beassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeAssignmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<BeAssignmentApplication>(*args)
+}
